7eb8e19dc339ce5868d6f04ec3df39175b33d35a
staslovelas <chertpaderi@gmail.com>
2020-08-13 15:58:03 +0300

    Fixed namespace and added two functions
    I added functions for random values here, they are needed in different classes, for example, DiamondSquareAlgorithm.cs and Experimenter.cs. Maybe later I will find better place for this code fragments.

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,22 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = System.Random;
 
-namespace DefaultNamespace
+public static class Utils
 {
-    public class Utils
+    public static Vector3 ChangeX(Vector3 v, float x)
     {
-        public static Vector3 ChangeX(Vector3 v, float x)
-        {
-            return new Vector3(x, v.y, v.z);
-        }
+        return new Vector3(x, v.y, v.z);
+    }
         
-        public static Vector3 ChangeY(Vector3 v, float y)
-        {
-            return new Vector3(v.x, y, v.z);
-        }
+    public static Vector3 ChangeY(Vector3 v, float y)
+    {
+        return new Vector3(v.x, y, v.z);
+    }
         
-        public static Vector3 ChangeZ(Vector3 v, float z)
-        {
-            return new Vector3(v.x, v.y, z);
-        }
+    public static Vector3 ChangeZ(Vector3 v, float z)
+    {
+        return new Vector3(v.x, v.y, z);
+    }
+
+    public static float RandomFloat(float min, float max)
+    {
+        var rnd = new Random();
+        return (float) (min + rnd.NextDouble() / (max - min + 1));
+    }
+    
+    public static int RandomInt(float min, float max)
+    {
+        var rnd = new Random();
+        return Mathf.RoundToInt(min + rnd.Next() / (max - min + 1));
     }
 }
