c7b851e9ed71ac0f7151bd2e576ab18e373c865f
staslovelas <chertpaderi@gmail.com>
2020-08-19 20:08:53 +0300

    Moved to an separate folder GeneratorEditor

---
+++ Assets/Editor/GeneratorEditor/DiamondSquareGenerator.cs
@@ -0,0 +1,27 @@
+﻿using GenerationAlgorithms;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.GeneratorEditor
+{
+    public static class DiamondSquareGenerator
+    {
+        private static float _heightMultiplier = 1;
+        private static AnimationCurve _heightCurve =  AnimationCurve.Linear(0, 0, 1, 1);
+        private static float _r = 0.6f;
+        private static int _seed = 1;
+
+        public static void Draw()
+        {
+            _heightMultiplier = EditorGUILayout.Slider("Height Multiplier", _heightMultiplier, 1, 50);
+            _heightCurve = EditorGUILayout.CurveField("Height Curve", _heightCurve);
+            _r = EditorGUILayout.Slider("Roughness",_r, 0, 1);
+            _seed = Mathf.RoundToInt(EditorGUILayout.Slider("Seed",_seed, 1, 30));
+        }
+
+        public static Vector3[] Generate(int mapSize)
+        {
+            return DiamondSquareAlgorithm.GenerateDiamondSquareMap(mapSize, _r, _seed, _heightMultiplier, _heightCurve);
+        }
+    }
+}
---
+++ Assets/Editor/GeneratorEditor/MainGeneratorEditor.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Generator;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.GeneratorEditor
+{
+    [CustomEditor(typeof(GeneratorInspector))]
+    public class MainGeneratorEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var genInsp = (GeneratorInspector) target;
+            DrawDefaultInspector();
+
+            switch (genInsp.generationAlgorithm)
+            {
+                case GeneratorInspector.GenerationAlgorithm.PerlinNoise:
+                    PerlinNoiseGenerator.Draw();
+                    break;
+                case GeneratorInspector.GenerationAlgorithm.DiamondSquare:
+                    DiamondSquareGenerator.Draw();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            switch (genInsp.optimizationMethod)
+            {
+                case GeneratorInspector.Optimization.LevelOfDetail:
+                    LevelOfDetailOptimizer.Draw(genInsp.mapSize);
+                    break;
+                case GeneratorInspector.Optimization.RamerDouglasPecker:
+                    RamerDouglasPeckerOptimizer.Draw();
+                    break;
+                case GeneratorInspector.Optimization.None:
+                    EditorGUILayout.Space();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            if (GUILayout.Button("Generate"))
+            {
+                switch (genInsp.generationAlgorithm)
+                {
+                    case GeneratorInspector.GenerationAlgorithm.PerlinNoise:
+                    {
+                        var heightMap = PerlinNoiseGenerator.Generate(genInsp.mapSize);
+                        switch (genInsp.optimizationMethod)
+                        {
+                            case GeneratorInspector.Optimization.LevelOfDetail:
+                            {
+                                var optimizedMap = LevelOfDetailOptimizer.Optimize(heightMap);
+                                genInsp.Generate(optimizedMap);
+                                break;
+                            }
+                            case GeneratorInspector.Optimization.RamerDouglasPecker:
+                            {
+                                var optimizedMap = RamerDouglasPeckerOptimizer.Optimize(heightMap);
+                                genInsp.Generate(optimizedMap);
+                                break;
+                            }
+                            case GeneratorInspector.Optimization.None:
+                            {
+                                genInsp.Generate(heightMap);
+                                break;
+                            }
+                        }
+
+                        break;
+                    }
+
+                    case GeneratorInspector.GenerationAlgorithm.DiamondSquare:
+                    {
+                        var heightMap= DiamondSquareGenerator.Generate(genInsp.mapSize);
+                        switch (genInsp.optimizationMethod)
+                        {
+                            case GeneratorInspector.Optimization.LevelOfDetail:
+                            {
+                                var optimizedMap = LevelOfDetailOptimizer.Optimize(heightMap);
+                                genInsp.Generate(optimizedMap);
+                                break;
+                            }
+                            case GeneratorInspector.Optimization.RamerDouglasPecker:
+                            {
+                                var optimizedMap = RamerDouglasPeckerOptimizer.Optimize(heightMap);
+                                genInsp.Generate(optimizedMap);
+                                break;
+                            }
+                            case GeneratorInspector.Optimization.None:
+                            {
+                                genInsp.Generate(heightMap);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+}
