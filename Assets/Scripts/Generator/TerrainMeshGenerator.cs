f139fb5fe98d82ff2d7c93bd1f848512350ea66d
staslovelas <chertpaderi@gmail.com>
2020-08-19 20:53:00 +0300

    Names are fixed, CreateVerts is deleted, LOD is removed
    LevelOfDetail stuff was moved to an separate classes and now TerrainMeshGenerator.cs generates mesh using heightMap only.
    Also, the function CreateVerts has been removed because it is redundant. There is no need to create vertices because heightMap is mesh points.

--- Assets/Scripts/Generator/TerrainMeshGenerator.cs
+++ Assets/Scripts/Generator/TerrainMeshGenerator.cs
@@ -1,92 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
-using DefaultNamespace;
 using UnityEngine;
 
 namespace Generator
 {
-    public class TerrainMeshGenerator
+    public static class TerrainMeshGenerator
     {
-        private static int[] vertIdsOfTriangles;
-        private static List<Vector3> vertices;
-        private static List<Vector2> uvCoords;
+        private static int[] _vertIdsOfTriangles;
+        private static List<Vector2> _uvCoords;
+        private static int _mapSize;
 
-        public static Mesh Generate(int meshSize, Vector3[] heightMap, float heightMultiplier, int lod, AnimationCurve heightCurve)
+        public static Mesh Generate(Vector3[] heightMap)
         {
-            var detailIncrement = lod == 0 ? 1 : lod * 2;
-            var verticesPerLine = (meshSize - 1) / detailIncrement + 1;
-
-            vertIdsOfTriangles = new int[(verticesPerLine - 1) * (verticesPerLine - 1) * 6];
-            vertices = new List<Vector3>();
-            uvCoords = new List<Vector2>();
-            var newMesh = new Mesh();
-
-            CreateVerts(meshSize, heightMap, heightMultiplier, detailIncrement, verticesPerLine, heightCurve);
-            newMesh.vertices = vertices.ToArray();
-
-            CreateTris(verticesPerLine);
-            newMesh.triangles = vertIdsOfTriangles;
+            _mapSize = (int) Math.Sqrt(heightMap.Length);
+            _vertIdsOfTriangles = new int[(_mapSize - 1) * (_mapSize - 1) * 6];
+            _uvCoords = new List<Vector2>();
+            
+            var newMesh = new Mesh {vertices = heightMap};
+            
+            CreateTris();
+            newMesh.triangles = _vertIdsOfTriangles;
             newMesh.RecalculateNormals();
 
-            CreateUvCoords(meshSize, detailIncrement);
-            newMesh.uv = uvCoords.ToArray();
+            CreateUvCoords();
+            newMesh.uv = _uvCoords.ToArray();
 
             return newMesh;
         }
 
-        private static void CreateVerts(int meshSize, Vector3[] heightMap, float heightMultiplier, int detailIncrement, int verticesPerLine, AnimationCurve heightCurve)
-        {
-            /*for (var y = 0; y < meshSize; y += detailIncrement)
-            {
-                for (var x = 0; x < meshSize; x += detailIncrement)
-                {
-                    vertices[currentVertIndex] =
-                        new Vector3(x, heightMap[x, y] * heightMultiplier, y);
-                    currentVertIndex++;
-                }
-            }*/
-
-            for (var i = 0; i < heightMap.Length; i += 1 + meshSize * (detailIncrement - 1))
-            {
-                for (var j = 0; j < verticesPerLine; j++)
-                {
-                    vertices.Add(new Vector3(heightMap[i].x, heightCurve.Evaluate(heightMap[i].y) * heightMultiplier, heightMap[i].z));
-                    i += detailIncrement;
-                }
-
-                i -= detailIncrement;
-            }
-        }
-
-        private static void CreateTris(int verticesPerLine)
+        private static void CreateTris()
         {
             var firstVertOfTris = 0;
             var v = 0;
 
-            for (var y = 0; y < verticesPerLine - 1; y++, v++)
+            for (var y = 0; y < _mapSize - 1; y++, v++)
             {
-                for (var x = 0; x < verticesPerLine - 1; x++, v++)
+                for (var x = 0; x < _mapSize - 1; x++, v++)
                 {
-                    vertIdsOfTriangles[firstVertOfTris] = v;
-                    vertIdsOfTriangles[firstVertOfTris + 1] = v + verticesPerLine;
-                    vertIdsOfTriangles[firstVertOfTris + 2] = v + verticesPerLine + 1;
+                    _vertIdsOfTriangles[firstVertOfTris] = v;
+                    _vertIdsOfTriangles[firstVertOfTris + 1] = v + _mapSize;
+                    _vertIdsOfTriangles[firstVertOfTris + 2] = v + _mapSize + 1;
 
-                    vertIdsOfTriangles[firstVertOfTris + 3] = v + verticesPerLine + 1;
-                    vertIdsOfTriangles[firstVertOfTris + 4] = v + 1;
-                    vertIdsOfTriangles[firstVertOfTris + 5] = v;
+                    _vertIdsOfTriangles[firstVertOfTris + 3] = v + _mapSize + 1;
+                    _vertIdsOfTriangles[firstVertOfTris + 4] = v + 1;
+                    _vertIdsOfTriangles[firstVertOfTris + 5] = v;
 
                     firstVertOfTris += 6;
                 }
             }
         }
 
-        private static void CreateUvCoords(int meshSize, int detailIncrement)
+        private static void CreateUvCoords()
         {
-            for (int i = 0, y = 0; y < meshSize; y += detailIncrement)
+            for (int i = 0, y = 0; y < _mapSize; y++)
             {
-                for (var x = 0; x < meshSize; x += detailIncrement, i++)
+                for (var x = 0; x < _mapSize; x++, i++)
                 {
-                    uvCoords.Add( new Vector2((float) x / meshSize, (float) y / meshSize));
+                    _uvCoords.Add( new Vector2((float) x / _mapSize, (float) y / _mapSize));
                 }
             }
         }
