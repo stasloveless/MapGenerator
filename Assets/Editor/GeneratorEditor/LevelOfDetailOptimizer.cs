ccc0ab9a9a332be23b6d46d31441b5f3d79dbc4e
staslovelas <chertpaderi@gmail.com>
2020-08-19 20:10:33 +0300

    Inspectors fo optimization algorithms were added
    For optimization algorithms, we also need to add inspector classes
    as for map generation algorithms (PerlinNoiseGenerator.cs and DiamondSquareGenerator.cs).

---
+++ Assets/Editor/GeneratorEditor/LevelOfDetailOptimizer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using LevelOfDetail;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.GeneratorEditor
+{
+    public static class LevelOfDetailOptimizer
+    {
+        private static int _levelOfDetail = 0;
+        private static List<int> _factors;
+        
+        public static void Draw(int meshSize)
+        {
+            _factors = MathCalculations.MathCalculations.CalculateFactors(meshSize - 1);
+            _levelOfDetail = EditorGUILayout.IntSlider("Level of Detail", _levelOfDetail, 0, _factors.Count - 1);
+        }
+
+        public static Vector3[] Optimize(Vector3[] heightMap)
+        {
+            var opt = SimpleLevelOfDetail.Optimize(heightMap, _factors[_levelOfDetail]);
+            return opt;
+        }
+    }
+}
---
+++ Assets/Editor/GeneratorEditor/RamerDouglasPeckerOptimizer.cs
@@ -0,0 +1,21 @@
+﻿using RamerDuglasPeucker3D;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.GeneratorEditor
+{
+    public static class RamerDouglasPeckerOptimizer
+    {
+        private static float _epsilon = 0.0f;
+        
+        public static void Draw()
+        {
+            _epsilon = EditorGUILayout.Slider("Epsilon", _epsilon, 0, 1);
+        }
+
+        public static Vector3[] Optimize(Vector3[] heightMap)
+        {
+            return RamerDuglasPeickerAlgorithm2D.OptimizeMap(heightMap, _epsilon);
+        }
+    }
+}
