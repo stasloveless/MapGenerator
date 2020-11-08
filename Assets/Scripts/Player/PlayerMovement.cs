d73926f5afbdfd03a7a8ab757ed85c149f83dfc7
staslovelas <chertpaderi@gmail.com>
2020-11-08 20:08:37 +0300

    PlayerMovement.cs was added
    A simple walk script

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController player;
+    public float speed = 10f;
+    public float gravity = -9.8f;
+    public float groundDistance = 0.4f;
+    public Transform groundChecker;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundChecker.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+        
+        var move = transform.right * x + transform.forward * z;
+        player.Move(move * Time.deltaTime * speed);
+
+        velocity.y += gravity * Time.deltaTime;
+        player.Move(velocity * Time.deltaTime);
+
+
+    }
+}
