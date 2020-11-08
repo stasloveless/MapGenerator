2c5035da61ddb1250daa29053ceffa1f51d84b7b
staslovelas <chertpaderi@gmail.com>
2020-11-08 17:11:30 +0300

    CameraController.cs was added
    It is needed for rotation player's view in the scene

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class CameraController : MonoBehaviour
+    {
+        private float xRotation = 0f;
+        public float mouseSensitivity = 100f;
+        public Transform cameraView;
+
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+            var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            
+            cameraView.Rotate(Vector3.up * mouseX);
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        }
+    }
+}
