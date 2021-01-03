96a57f547db2939d044e13234e223fe08ab9c88c
Anastasia Shevchenko <chertpaderi@gmail.com>
2021-01-03 20:38:04 +0300

    Reduced number of passed parameters in FromHeightMap()
    Now you don't need to pass the size of the mesh, it is calculated inside the function.

--- Assets/Scripts/Generator/CreateTexture.cs
+++ Assets/Scripts/Generator/CreateTexture.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Generator
 {
     public static class CreateTexture
     {
-        public static Texture2D FromHeightMap(Vector3[] heightMap, int mapSize)
+        public static Texture2D FromHeightMap(Vector3[] heightMap)
         {
+            var mapSize = (int) Math.Sqrt(heightMap.Length);
             var colorMap = new Color[mapSize * mapSize];
 
             for (var i = 0; i < colorMap.Length; i++)
