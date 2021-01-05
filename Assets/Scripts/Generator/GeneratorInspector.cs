6a49bd6ae3467d4c5e97e7e5468f3f634ccb6802
Anastasia Shevchenko <chertpaderi@gmail.com>
2021-01-05 19:00:21 +0300

    Move generator inspector interface to another class
    From MapGenerator.cs to new GeneratorInspector.cs

---
+++ Assets/Scripts/Generator/GeneratorInspector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace Generator
+{
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    public class GeneratorInspector : MonoBehaviour
+    {
+        public enum GenerationAlgorithm
+        {
+            PerlinNoise,
+            DiamondSquare
+        }
+        public enum Optimization
+        {
+            None,
+            LevelOfDetail,
+            RamerDouglasPecker
+        }
+
+        [Range(1, 255)] public int mapSize;
+
+        public GenerationAlgorithm generationAlgorithm = GenerationAlgorithm.PerlinNoise;
+        public Optimization optimizationMethod = Optimization.None;
+
+        
+        public void Generate(Vector3[] heightMap)
+        {
+            var mapGen = new MapGenerator(mapSize, heightMap);
+            Mesh terrainMesh;
+            //Texture2D terrainTexture;
+
+            terrainMesh = mapGen.GenerateMesh(optimizationMethod);
+            //terrainTexture = mapGen.GenerateTexture();
+            terrainMesh.name = "Procedural map";
+            //GetComponent<MeshRenderer>().sharedMaterial.mainTexture = terrainTexture;
+            GetComponent<MeshFilter>().mesh = terrainMesh;
+            GetComponent<MeshCollider>().sharedMesh = terrainMesh;
+
+            Debug.Log(terrainMesh.triangles.Length);
+        }
+    }
+}
--- Assets/Scripts/Generator/MapGenerator.cs
+++ Assets/Scripts/Generator/MapGenerator.cs
@@ -5,77 +5,55 @@ using Delaunay;
 using DelaunayTriangulator;
 using RamerDuglasPeucker3D;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Generator
 {
-    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-    public class MapGenerator : MonoBehaviour
+    public class MapGenerator
     {
-        public enum GenerationAlgorithm
+        public int mapSize;
+        public Vector3[] heightMap;
+
+        public MapGenerator(int mapSize, Vector3[] heightMap)
         {
-            PerlinNoise,
-            DiamondSquare
-        };
+            this.mapSize = mapSize;
+            this.heightMap = heightMap;
+        }
 
-        public bool delaunay;
-        [Range(1, 255)] public int mapSize;
-        [Range(0, 32)] public int levelOfDetail;
-
-        [Range(1, 100)] public float heightMultiplier;
-        public AnimationCurve heightCurve;
-        [Range(0.0001f, 1)] public float epsilon;
-        //public bool pseudoDistance;
-        //[Range(0f, 1f)] public float c;
-        
-        public GenerationAlgorithm generationAlgorithm = GenerationAlgorithm.PerlinNoise;
-        //public OptimizationAlgorithm optimizationAlgorithm
-
-        public void Generate(Vector3[] heightMap)
+        public Mesh GenerateMesh(GeneratorInspector.Optimization optimizationMethod)
         {
-            var terrainMesh = new Mesh();
-            var terrainTexture = Texture2D.whiteTexture;
-            var triangulation = new List<Triangle>();
-
-            if (delaunay)
+            Mesh terrainMesh;
+            
+            switch (optimizationMethod)
             {
-                var optimizedHeightMap = RamerDuglasPeickerAlgorithm2D.OptimizeMap(heightMap, epsilon, mapSize);
-                var optimizedVector2Map = ExtractXZToIntVector2(optimizedHeightMap);
-                var triangulator = new Triangulator();
-                var triangles = triangulator.Triangulation(optimizedVector2Map);
-                triangulation = TriadsToTriangles(triangles, optimizedVector2Map);
-                terrainMesh = IrregularTerrainMeshGenerator.Generate(optimizedHeightMap, triangulation, heightMultiplier, mapSize, heightCurve);
-                terrainTexture = CreateTexture.FromHeightMap(heightMap, mapSize);
-            }
-            else
-            {
-                switch (generationAlgorithm)
+                case GeneratorInspector.Optimization.LevelOfDetail:
                 {
-                    case GenerationAlgorithm.PerlinNoise:
-                    {
-                        terrainMesh =
-                            TerrainMeshGenerator.Generate(mapSize, heightMap, heightMultiplier, levelOfDetail, heightCurve);
-                        terrainTexture = CreateTexture.FromHeightMap(heightMap, mapSize);
-                        break;
-                    }
-                    case GenerationAlgorithm.DiamondSquare:
-                    {
-                        terrainMesh =
-                            TerrainMeshGenerator.Generate(mapSize, heightMap, heightMultiplier, levelOfDetail, heightCurve);
-                        terrainTexture = CreateTexture.FromHeightMap(heightMap, mapSize);
-                        break;
-                    }
-                    default:
-                        throw new ArgumentOutOfRangeException();
+                    terrainMesh = TerrainMeshGenerator.Generate(heightMap);
+                    break;
                 }
+                case GeneratorInspector.Optimization.RamerDouglasPecker:
+                {
+                    var optimizedVector2Map = ExtractXZToIntVector2(heightMap);
+                    var triangulator = new Triangulator();
+                    var triangles = triangulator.Triangulation(optimizedVector2Map);
+                    var triangulation = TriadsToTriangles(triangles, optimizedVector2Map);
+                    terrainMesh = IrregularTerrainMeshGenerator.Generate(heightMap, triangulation, mapSize);
+                    break;
+                }
+                case GeneratorInspector.Optimization.None:
+                {
+                    terrainMesh = TerrainMeshGenerator.Generate(heightMap);
+                    break;
+                }
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
             
-            GetComponent<MeshRenderer>().sharedMaterial.mainTexture = terrainTexture;
-            GetComponent<MeshFilter>().mesh = terrainMesh;
-            terrainMesh.name = "Procedural Grid";
-            
-            //Debug.Log("Vertices HP: " + terrainMesh.vertices.Length);
-            Debug.Log("Tris HP: " + terrainMesh.triangles.Length / 3);
+            return terrainMesh;
+        }
+
+        public Texture2D GenerateTexture()
+        {
+            return CreateTexture.FromHeightMap(heightMap);
         }
 
         private List<Triangle> TriadsToTriangles(List<Triad> triangles, List<Vector2> heightMap)
@@ -102,7 +80,7 @@ namespace Generator
 
             return result.ToArray();
         }
-        
+
         public static List<Vector2> ExtractXZToIntVector2(Vector3[] array)
         {
             return array.Select(element => new Vector2(element.x, element.z)).ToList();
