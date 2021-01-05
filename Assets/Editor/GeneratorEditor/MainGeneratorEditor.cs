440d8477044d3948b04d8963882ccc091af38b6e
Anastasia Shevchenko <chertpaderi@gmail.com>
2021-01-05 20:33:59 +0300

    Add generator parameters
    I want to remove GeneratorInspector.cs from project and simplify code in future.

--- Assets/Editor/GeneratorEditor/MainGeneratorEditor.cs
+++ Assets/Editor/GeneratorEditor/MainGeneratorEditor.cs
@@ -8,32 +8,68 @@ namespace Editor.GeneratorEditor
     [CustomEditor(typeof(GeneratorInspector))]
     public class MainGeneratorEditor : UnityEditor.Editor
     {
+        public enum GenerationAlgorithm
+        {
+            PerlinNoise,
+            DiamondSquare,
+            None
+        }
+        public enum Optimization
+        {
+            None,
+            LevelOfDetail,
+            RamerDouglasPecker
+        }
+
+        public int mapSize;
+        public bool importHeightMap;
+
+        public GenerationAlgorithm generationAlgorithm = GenerationAlgorithm.None;
+        public Optimization optimizationMethod = Optimization.None;
+
+        public Texture2D externalHeightMap;
+
         public override void OnInspectorGUI()
         {
             var genInsp = (GeneratorInspector) target;
-            DrawDefaultInspector();
+            generationAlgorithm = (GenerationAlgorithm)EditorGUILayout.EnumPopup("Generation Algorithm",generationAlgorithm);
+            importHeightMap = EditorGUILayout.Toggle("Import Height Map", importHeightMap);
+            optimizationMethod = (Optimization)EditorGUILayout.EnumPopup("Optimization Algorithm",optimizationMethod);
+            mapSize = Mathf.RoundToInt(EditorGUILayout.Slider("Map size", mapSize, 1, 255));
 
-            switch (genInsp.generationAlgorithm)
+            if (importHeightMap)
             {
-                case GeneratorInspector.GenerationAlgorithm.PerlinNoise:
+                generationAlgorithm = GenerationAlgorithm.None;
+                externalHeightMap = (Texture2D) EditorGUILayout.ObjectField("Height Map", externalHeightMap, typeof(Texture2D), false);
+
+            }
+            
+            //DrawDefaultInspector();
+
+            switch (generationAlgorithm)
+            {
+                case GenerationAlgorithm.PerlinNoise:
                     PerlinNoiseGenerator.Draw();
                     break;
-                case GeneratorInspector.GenerationAlgorithm.DiamondSquare:
+                case GenerationAlgorithm.DiamondSquare:
                     DiamondSquareGenerator.Draw();
+                    break;
+                case GenerationAlgorithm.None:
+                    EditorGUILayout.Space();
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
             }
 
-            switch (genInsp.optimizationMethod)
+            switch (optimizationMethod)
             {
-                case GeneratorInspector.Optimization.LevelOfDetail:
+                case Optimization.LevelOfDetail:
                     LevelOfDetailOptimizer.Draw(genInsp.mapSize);
                     break;
-                case GeneratorInspector.Optimization.RamerDouglasPecker:
+                case Optimization.RamerDouglasPecker:
                     RamerDouglasPeckerOptimizer.Draw();
                     break;
-                case GeneratorInspector.Optimization.None:
+                case Optimization.None:
                     EditorGUILayout.Space();
                     break;
                 default:
@@ -42,26 +78,26 @@ namespace Editor.GeneratorEditor
 
             if (GUILayout.Button("Generate"))
             {
-                switch (genInsp.generationAlgorithm)
+                switch (generationAlgorithm)
                 {
-                    case GeneratorInspector.GenerationAlgorithm.PerlinNoise:
+                    case GenerationAlgorithm.PerlinNoise:
                     {
                         var heightMap = PerlinNoiseGenerator.Generate(genInsp.mapSize);
-                        switch (genInsp.optimizationMethod)
+                        switch (optimizationMethod)
                         {
-                            case GeneratorInspector.Optimization.LevelOfDetail:
+                            case Optimization.LevelOfDetail:
                             {
                                 var optimizedMap = LevelOfDetailOptimizer.Optimize(heightMap);
                                 genInsp.Generate(optimizedMap);
                                 break;
                             }
-                            case GeneratorInspector.Optimization.RamerDouglasPecker:
+                            case Optimization.RamerDouglasPecker:
                             {
                                 var optimizedMap = RamerDouglasPeckerOptimizer.Optimize(heightMap, PerlinNoiseGenerator._heightMultiplier);
                                 genInsp.Generate(optimizedMap);
                                 break;
                             }
-                            case GeneratorInspector.Optimization.None:
+                            case Optimization.None:
                             {
                                 genInsp.Generate(heightMap);
                                 break;
@@ -71,24 +107,24 @@ namespace Editor.GeneratorEditor
                         break;
                     }
 
-                    case GeneratorInspector.GenerationAlgorithm.DiamondSquare:
+                    case GenerationAlgorithm.DiamondSquare:
                     {
                         var heightMap= DiamondSquareGenerator.Generate(genInsp.mapSize);
-                        switch (genInsp.optimizationMethod)
+                        switch (optimizationMethod)
                         {
-                            case GeneratorInspector.Optimization.LevelOfDetail:
+                            case Optimization.LevelOfDetail:
                             {
                                 var optimizedMap = LevelOfDetailOptimizer.Optimize(heightMap);
                                 genInsp.Generate(optimizedMap);
                                 break;
                             }
-                            case GeneratorInspector.Optimization.RamerDouglasPecker:
+                            case Optimization.RamerDouglasPecker:
                             {
                                 var optimizedMap = RamerDouglasPeckerOptimizer.Optimize(heightMap, DiamondSquareGenerator._heightMultiplier);
                                 genInsp.Generate(optimizedMap);
                                 break;
                             }
-                            case GeneratorInspector.Optimization.None:
+                            case Optimization.None:
                             {
                                 genInsp.Generate(heightMap);
                                 break;
