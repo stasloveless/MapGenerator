b585a63b5a94c0731f85b60d6bef542e5f1491a1
Anastasia Shevchenko <chertpaderi@gmail.com>
2021-03-03 15:46:22 +0300

    Variable name fix

--- Assets/Editor/GeneratorEditor/DiamondSquareGenerator.cs
+++ Assets/Editor/GeneratorEditor/DiamondSquareGenerator.cs
@@ -6,14 +6,14 @@ namespace Editor.GeneratorEditor
 {
     public static class DiamondSquareGenerator
     {
-        public static float _heightMultiplier = 1;
+        public static float heightMultiplier = 1;
         private static AnimationCurve _heightCurve =  AnimationCurve.Linear(0, 0, 1, 1);
         private static float _r = 0.6f;
         private static int _seed = 1;
 
         public static void Draw()
         {
-            _heightMultiplier = EditorGUILayout.Slider("Height Multiplier", _heightMultiplier, 1, 50);
+            heightMultiplier = EditorGUILayout.Slider("Height Multiplier", heightMultiplier, 1, 50);
             _heightCurve = EditorGUILayout.CurveField("Height Curve", _heightCurve);
             _r = EditorGUILayout.Slider("Roughness",_r, 0, 1);
             _seed = Mathf.RoundToInt(EditorGUILayout.Slider("Seed",_seed, 1, 30));
@@ -21,7 +21,7 @@ namespace Editor.GeneratorEditor
 
         public static Vector3[] Generate(int mapSize)
         {
-            return DiamondSquareAlgorithm.GenerateDiamondSquareMap(mapSize, _r, _seed, _heightMultiplier, _heightCurve);
+            return DiamondSquareAlgorithm.GenerateDiamondSquareMap(mapSize, _r, _seed, heightMultiplier, _heightCurve);
         }
     }
 }
